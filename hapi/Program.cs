a0ffee13e25736028f379fe7bb8aa10506d09022
Łukasz Gut <blinekgames@gmail.com>
2020-05-20 14:34:15 +0200

    Create project

---
+++ hapi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
