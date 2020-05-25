2666393f5ee45905aa2c1b9f1e6d1ca19b74aab0
Łukasz Gut <blinku.dev@gmail.com>
2020-05-25 19:27:17 +0200

    Start implementing CLI example

---
+++ example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Linq;
+using hapi.company;
+using hapi.context;
+
+namespace example
+{
+    public class Program
+    {
+        private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HierarchyDB;Integrated Security=True";
+
+        public static void Main(string[] args)
+        {
+            var connectionContext = new ConnectionContext(ConnectionString);
+
+            var document = XDocument.Parse(
+                @"<Company xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""/>");
+
+            var companyDao = new CompanyDAO(connectionContext);
+            companyDao.AddCompany(new Company("Example Co.", document));
+
+            var company = companyDao.GetCompanyByName("Example Co.");
+            Console.WriteLine(company);
+
+            companyDao.RemoveCompanyByName("Example Co.");
+        }
+    }
+}
