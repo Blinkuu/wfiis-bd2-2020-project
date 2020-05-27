427e328d62e20ef31d351335db3d966176a8c37e
Łukasz Gut <blinku.dev@gmail.com>
2020-05-27 14:11:50 +0200

    Change file name

---
+++ hapi/context/ConnectionContext.cs
@@ -0,0 +1,20 @@
+﻿namespace hapi.context
+{
+    /// <summary>
+    ///     <c>ConnectionContext</c> class.
+    ///     Przechowuje string służący do łączenia się z bazą danych.
+    /// </summary>
+    public class ConnectionContext
+    {
+        /// <summary>
+        ///     Tworzy nową instancję instance klasy <see cref="ConnectionContext" />.
+        /// </summary>
+        /// <param name="connectionString">String do połączenia się z bazą danych.</param>
+        public ConnectionContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public string connectionString { get; set; }
+    }
+}
